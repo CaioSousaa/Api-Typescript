bc13ca5ef8cff8562543ee15382bf2a477ac6d93
Caio <caiothalles607@gmail.com>
2023-10-25 21:49:40 -0300

    refactoring the listing

---
+++ src/modules/useCases/ListCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,12 @@
+import { Category } from "../../cars/model/Category";
+import { ICategoriesRepository } from "../../cars/repositories/ICategoriesRepository";
+
+class ListCategoriesUseCase {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+    execute(): Category[] {
+        const categories = this.categoriesRepository.list();
+        return categories;
+    }
+}
+
+export { ListCategoriesUseCase };
